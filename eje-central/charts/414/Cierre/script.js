90f66bb97da06f56b1cf2b43d1582972ec624206
kevin-cartografia <kevingutierrez.cartografia@gmail.com>
2024-08-30 14:51:40 -0600

    Update script.js

--- eje-central/charts/414/Cierre/script.js
+++ eje-central/charts/414/Cierre/script.js
@@ -1,28 +1,28 @@
-// Selecciona el modal
+// Obtén el modal
 var modal = document.getElementById("modal");
 
-// Selecciona el elemento <span> que cierra el modal
-var span = document.getElementsByClassName("close")[0];
-
-// Selecciona todos los marcadores
+// Obtén el botón que abre el modal
 var markers = document.querySelectorAll('.marker');
 
-// Añade eventos de clic a los marcadores para mostrar el modal con su contenido
-markers.forEach(function(marker) {
+// Obtén el elemento <span> que cierra el modal
+var span = document.querySelector(".close");
+
+// Cuando el usuario haga clic en un marcador, abre el modal
+markers.forEach(marker => {
     marker.onclick = function() {
         var popupId = this.getAttribute('data-popup');
         var popupContent = document.getElementById(popupId).innerHTML;
-        document.getElementById("modal-text").innerHTML = popupContent;
+        document.getElementById('modal-text').innerHTML = popupContent;
         modal.style.display = "flex";
     }
 });
 
-// Cuando el usuario hace clic en <span> (la "X"), cierra el modal
+// Cuando el usuario haga clic en <span> (x), cierra el modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Cuando el usuario hace clic fuera del contenido del modal, cierra el modal
+// Cuando el usuario haga clic en cualquier lugar fuera del modal, ciérralo
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
